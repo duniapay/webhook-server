398287a24d82c14d0d6292c58fdc882dc9c3e4b4
serge <sergewilfried@outlook.fr>
2023-02-03 12:50:16 +0000

    feat: use redis queues

--- index.js
+++ index.js
@@ -1,87 +1,51 @@
-const { Kafka } = require('kafkajs')
 const axios = require('axios')
 const { createHmac } = import('crypto');
+const RedisSMQ = require("rsmq");
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const FROM_BEGINNING = process.env.NODE_ENV !== 'production' ? true : false
-
-
+/// Load private keys from environment variable
+const REDIS_HOST =
+  process.env.NODE_ENV === 'localhost'
+    ? 'redis://localhost:6379'
+    : process.env.REDISCLOUD_URL
+    
+    const raw_url = new URL(REDIS_HOST)
+    const REDIS_HOSTNAME = raw_url.hostname
+    const REDIS_PORT = raw_url.port
+    const REDIS_PASSWORD = raw_url.password
+    const NAMESPACE = 'rsmq'
+    
+    const rsmq = new RedisSMQ({
+      host: REDIS_HOSTNAME,
+      port: REDIS_PORT,
+      ns: NAMESPACE,
+      password: REDIS_PASSWORD,
+    })
 
   async function main() {
-    let kafka;
-
-    if(process.env.NODE_ENV !== 'production') {
-      kafka = new Kafka({
-        clientId: 'fiatconnect',
-        brokers: ['localhost:9092'],
-      })
+    await listenFromQueue('kyc');
+    await listenFromQueue('transfer');
+}
     
-      const producer = kafka.producer()
-      await producer
-        .connect()
-        .then(() => {
-          logger.info('Broker has been initialized')
-        })
-        .catch((e) => {
-          logger.error(e)
-        })
-    } else {
-      const broker = process.env.KAFKA_URL
-      // convert this string to array
-      const brokersList = broker.split(',')
 
-      kafka = new Kafka({
-        clientId: 'fiatconnect',
-        brokers: brokersList,
-        ssl: {
-          rejectUnauthorized: false,
-          ca: process.env.KAFKA_TRUSTED_CERT,
-          key: process.env.KAFKA_CLIENT_CERT_KEY,
-          cert: process.env.KAFKA_CLIENT_CERT,
-        },
-      })
-    
-      const producer = kafka.producer()
-      await producer
-        .connect()
-        .then(() => {
-          logger.info('Broker has been initialized')
-        })
-        .catch((e) => {
-          logger.error(e)
-        })
-      return producer
+  
+  
+async function listenFromQueue(queueName) {
+  rsmq.receiveMessage({ qname: queueName }, async function (err, resp) {
+    if (err) {
+      console.error(err)
+      return
     }
-    
-
-    const consumer = kafka.consumer({ groupId: 'webhook-server' })
-    await consumer.connect()
-    await consumer.subscribe({ topics: ['kyc' , 'transfer'], fromBeginning: FROM_BEGINNING })
-
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        console.log({
-          value: message.value.toString(),
-          topic: topic,
-          
-        })
-        let response = await sendEvent(message.value.toString())
-
-        // Retry logic for failed requests
-        let retry_count = 0
-        const retry_intervals = [3, 66, 731, 4098]
-        // while response.status_code != 200 and retry_count < 4
-       while (response.status !==200 && retry_count<4) {
-          retry_count += 1
-          await new Promise((resolve) => setTimeout(resolve, retry_intervals[retry_count-1]))
-          response = await sendEvent(message.value.toString())
-          return response;
-       }
-      },
-    })
-    return consumer;
-  }
-
+    if (resp.id) {
+      console.log("Message received.", resp)
+      await sendEvent(resp.message)
+    } else {
+      console.log("No messages for me...")
+    }
+  });
+}
 
   async function sendEvent(e) {
         const providerId = 'dunia-payment'
