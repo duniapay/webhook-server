a03b8b84e02a34e1b49287c8ed51f674132d2345
serge <sergewilfried@outlook.fr>
2023-05-21 11:53:36 +0000

    🔥 chore(.gitignore): add .env file to gitignore 🐳 chore(Dockerfile): add Dockerfile for building and running the app in a container 🔥 chore(Procfile, app.json): remove unused files 🔧 chore(fly.toml): add fly.toml configuration file for deploying the app to fly.io 🎨 style(server.ts): change port variable case from lowercase port to uppercase PORT 🐛 fix(index.js): fix error handling when sending webhook event ✨ feat(index.js): add support for sending webhook events to a remote server using Redis and Bull

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
-let throng = require("throng");
-let Queue = require("bull");
+const Queue = require('bull');
+const axios = require('axios');
+require('dotenv').config()
 
-// Connect to a local redis instance locally, and the Heroku-provided URL in production
-let REDIS_URL = process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379";
+const webhookQueue = new Queue('webhook', process.env.REDIS_URL);
 
-// Spin up multiple processes to handle jobs to take advantage of more CPU cores
-// See: https://devcenter.heroku.com/articles/node-concurrency for more info
-let workers = process.env.WEB_CONCURRENCY || 2;
+webhookQueue.process(async (job) => {
+  const event = job.data;
 
-// The maximum number of jobs each worker should process at once. This will need
-// to be tuned for your application. If each job is mostly waiting on network
-// responses it can be much higher. If each job is CPU-intensive, it might need
-// to be much lower.
-let maxJobsPerWorker = 50;
+  try {
+    const response = await axios.post(process.env.WEBHOOK_SERVER_URL, event);
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function start() {
-  // Connect to the named work queue
-  // If you want to pass some "custom" Redis config
-  const workQueue = new Queue("kyc", {
-    redis: REDIS_URL,
-  });
-  workQueue.process(maxJobsPerWorker, async (job) => {
-    // This is an example job that just slowly reports on progress
-    // while doing no work. Replace this with your own job logic.
-    let progress = 0;
-
-    // throw an error 5% of the time
-    if (Math.random() < 0.05) {
-      throw new Error("This job failed!");
+    if (response.status !== 200) {
+      throw new Error(`Received status code ${response.status}`);
     }
+  } catch (error) {
+    throw new Error('Error sending webhook event');
+  }
+});
 
-    while (progress < 100) {
-      await sleep(50);
-      progress += 1;
-      job.progress(progress);
-    }
-
-    // A job can return values that will be stored in Redis as JSON
-    // This return value is unused in this demo application.
-    return { value: "This will be stored" };
+async function sendWebhookEvent(event) {
+  await webhookQueue.add(event, {
+    attempts: 4,
+    backoff: {
+      type: 'exponential',
+      delay: 1000
+    },
+    removeOnComplete: true
   });
 }
-
-// Initialize the clustered worker process
-// See: https://devcenter.heroku.com/articles/node-concurrency for more info
-throng({ workers, start });
